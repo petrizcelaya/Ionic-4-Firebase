023d11a4d28b11d8e034d1b212acbf2a5e73859c
Petriz Celaya <celaya93_12@hotmail.com>
2019-08-15 11:03:15 -0500

    firebase config

--- src/environments/firebaseconfig.ts
+++ src/environments/firebaseconfig.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAFFLY9QGsVT3ZZA6ssQEmNYdLjBZCnm10",
-    authDomain: "ionicfirebasetallercf.firebaseapp.com",
-    databaseURL: "https://ionicfirebasetallercf.firebaseio.com",
-    projectId: "ionicfirebasetallercf",
-    storageBucket: "ionicfirebasetallercf.appspot.com",
-    messagingSenderId: "323422179040",
-    appId: "1:323422179040:web:abe06f7b3b27dec6"
+    // Ingresa aquí tu configuración de Firebase
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
 };
